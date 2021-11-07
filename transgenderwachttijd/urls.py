837002f4a8a9b1ceb970ee97ca0cd891848ce9c8
Daniel Huisman <daniel@huisman.me>
2021-11-07 16:36:35 +0100

    Remove templates and views

--- transgenderwachttijd/urls.py
+++ transgenderwachttijd/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = i18n_patterns(
-    path('', include('providers.urls')),
     path('admin/', admin.site.urls),
 )
