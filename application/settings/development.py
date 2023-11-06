3f34450f38256a21fea97f853241f4d2336ad760
Danielle Huisman <danielle@huisman.me>
2023-11-06 11:46:19 +0100

    Fix CORS in development settings

--- application/settings/development.py
+++ application/settings/development.py
@@ -7,12 +7,12 @@ DEBUG = True
 
 SECRET_KEY = '5cw1)q4ww+ckr5wevw@wr2zymp57$k@4fy3*@h2%(l79bx5b9i'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Cross-Origin Resource Sharing (CORS)
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 
 # Cross Site Request Forgery (CSRF)
