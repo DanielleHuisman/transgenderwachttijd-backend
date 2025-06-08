3a613a60170f3de6e7c44d96e2a069f78d1df361
Daniëlle Huisman <danielle@huisman.me>
2025-06-08 11:06:10 +0200

    Remove OOG scraper

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from transgenderwachttijd.providers.scrapers.amc import ScraperAMC
 from transgenderwachttijd.providers.scrapers.devaart import ScraperDeVaart
 from transgenderwachttijd.providers.scrapers.genderhealthcare import ScraperGenderhealthcare
 from transgenderwachttijd.providers.scrapers.jonx import ScraperJonx
-from transgenderwachttijd.providers.scrapers.oog import ScraperOOG
 from transgenderwachttijd.providers.scrapers.psytrans import ScraperPsyTrans
 from transgenderwachttijd.providers.scrapers.radboudumc import ScraperRadboudumc
 from transgenderwachttijd.providers.scrapers.umcg import ScraperUMCG
@@ -22,8 +21,6 @@ def test(scraper_name: str):
         scraper = ScraperGenderhealthcare()
     elif scraper_name == 'jonx':
         scraper = ScraperJonx()
-    elif scraper_name == 'oog':
-        scraper = ScraperOOG()
     elif scraper_name == 'psytrans':
         scraper = ScraperPsyTrans()
     elif scraper_name == 'radboudumc':
--- transgenderwachttijd/providers/scrapers/__init__.py
+++ transgenderwachttijd/providers/scrapers/__init__.py
@@ -5,7 +5,6 @@ from transgenderwachttijd.providers.scrapers.amc import ScraperAMC
 from transgenderwachttijd.providers.scrapers.devaart import ScraperDeVaart
 from transgenderwachttijd.providers.scrapers.genderhealthcare import ScraperGenderhealthcare
 from transgenderwachttijd.providers.scrapers.jonx import ScraperJonx
-from transgenderwachttijd.providers.scrapers.oog import ScraperOOG
 from transgenderwachttijd.providers.scrapers.psytrans import ScraperPsyTrans
 from transgenderwachttijd.providers.scrapers.radboudumc import ScraperRadboudumc
 from transgenderwachttijd.providers.scrapers.umcg import ScraperUMCG
@@ -15,7 +14,6 @@ scrapers: List[Scraper] = [
     ScraperDeVaart(),
     ScraperGenderhealthcare(),
     ScraperJonx(),
-    ScraperOOG(),
     ScraperPsyTrans(),
     ScraperRadboudumc(),
     ScraperUMCG(),
